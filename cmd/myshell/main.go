2c1ce7d13759f1b2681a74bcf1fb66aa031b2232
primequantuM4 <gediezra@gmail.com>
2024-06-16 13:44:46 +0300

    stage 2 command not found

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,15 +4,23 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	// fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	if err != nil {
+		fmt.Printf("Something went wrong when reading file: %#v\n", err)
+		return
+	}
+
+	command = strings.TrimSpace(command)
+	fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
 }
